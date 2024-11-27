9b1f010c21e3af5abf32e94c403fa6c706dbc43d
Georg Dronov <dronovgeorg72@gmail.com>
2024-11-27 15:11:29 +0300

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="container mt-5">
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='container mt-5'>
+        <h1>Login Page</h1>
+    </div>
+  )
+}
